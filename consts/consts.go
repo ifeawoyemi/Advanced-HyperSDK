03c2f5507d6bd30094176b515991ca045a47a7fb
ifeawoyemi <ifeawoyemi@gmail.com>
2024-06-18 07:05:55 +0100

    initial commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "token"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Ifeoluwa"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "IVY"
 )
 
 var ID ids.ID
